25f039f330d531668fb5270f63dacd83a5d22676
gregmedlock <gmedlo@gmail.com>
2020-04-18 13:29:02 -0400

    completed first fit of bayesian regression with MAP. Required copy/paste workaround on formulas... but works.

--- bin/isme2016_growth_modeling.R
+++ bin/isme2016_growth_modeling.R
@@ -1,3 +1,5 @@
+library(zoo) # AUC calculation
+
 
 # read in the data from Biggs et al., ISME 2016
 relativeIntensities = read.table('/home/glm5uh/asf_characterization/Metabolomics/NMR_Integrals/Integrals_UVA.csv',
@@ -39,9 +41,11 @@ rowNames_RI_media_centered = RI_media_centered[,1]
 #rename RI sample labels to match the growth curves
 RI_media_centered$Name = paste0(sampleInfo$Grower,'in',sampleInfo$Medium,"_",sampleInfo$Rep)
 
-### filter out the samples that didn't contain ASF519, the only strain we'll model for now
+### filter out the samples that didn't contain ASF519
 RI_media_centered_519 = RI_media_centered[startsWith(RI_media_centered$Name,'519'),]
 
+### filter out the samples that didn't contain ASF356
+RI_media_centered_356 = RI_media_centered[startsWith(RI_media_centered$Name,'356'),]
 
 ### Get the outcome data. We are going to try the AUC for each sample's growth curve.
 # This function interpolates all growth curves to a common reference.
@@ -97,25 +101,26 @@ gc = read.table(fileName,sep='\t',header=F,row.names=NULL)
 gcdf = data.frame(gc)
 names(gcdf) = c("time","OD","group")
 t1 = gcdf$time[gcdf$group == 1]
-od1 = gcdf$OD[gcdf$group == 1]
-t2 = gcdf$time[gcdf$group == 2]
-od2 = gcdf$OD[gcdf$group == 2]
-t3 = gcdf$time[gcdf$group == 3]
-od3 = gcdf$OD[gcdf$group == 3]
-t4 = gcdf$time[gcdf$group == 4]
-od4 = gcdf$OD[gcdf$group == 4]
-# set zero/negative values as an arbitrarily low positive value.
-od1 = correctTime(od1,t1,t1)
-od2 = correctTime(od2,t2,t1)
-od3 = correctTime(od3,t3,t1)
-od4 = correctTime(od4,t4,t1)
-ODs = matrix(0,ncol=4,nrow=length(t1))
-ODs[,1] = od1
-ODs[,2] = od2
-ODs[,3] = od3
-ODs[,4] = od4
-ODs = data.frame(ODs)
-names(ODs) = paste0("ASF519in0_",c("1","2","3","4"))
+ODs_519 = collectGrowthCurves(fileName,t1)
+# od1 = gcdf$OD[gcdf$group == 1]
+# t2 = gcdf$time[gcdf$group == 2]
+# od2 = gcdf$OD[gcdf$group == 2]
+# t3 = gcdf$time[gcdf$group == 3]
+# od3 = gcdf$OD[gcdf$group == 3]
+# t4 = gcdf$time[gcdf$group == 4]
+# od4 = gcdf$OD[gcdf$group == 4]
+# # set zero/negative values as an arbitrarily low positive value.
+# od1 = correctTime(od1,t1,t1)
+# od2 = correctTime(od2,t2,t1)
+# od3 = correctTime(od3,t3,t1)
+# od4 = correctTime(od4,t4,t1)
+# ODs = matrix(0,ncol=4,nrow=length(t1))
+# ODs[,1] = od1
+# ODs[,2] = od2
+# ODs[,3] = od3
+# ODs[,4] = od4
+# ODs = data.frame(ODs)
+# names(ODs) = paste0("ASF519in0_",c("1","2","3","4"))
 
 timeRef = t1
 for (condition in paste0("ASF519in",c("356","360","361","492","500","502","519")))
@@ -123,15 +128,38 @@ for (condition in paste0("ASF519in",c("356","360","361","492","500","502","519")
   str(condition)
   fileName = paste0("/home/glm5uh/asf_characterization/GrowthCurves/SpentMediaGrowthCurveData/",condition,".tsv")
   new_ODs = collectGrowthCurves(fileName,timeRef=timeRef)
-  ODs = cbind(ODs,new_ODs)
-  str(names(ODs))
+  ODs_519 = cbind(ODs_519,new_ODs)
+}
+
+# calculate AUCs
+AUCs_519 = apply(ODs_519,2,function(x) sum(diff(timeRef)*rollmean(abs(x-0.001),2)))
+# calculate max
+maxes_519 = apply(ODs_519,2,function(x) max(x))
+
+
+# Repeat growth curve processing for ASF356
+fileName = "/home/glm5uh/asf_characterization/GrowthCurves/SpentMediaGrowthCurveData/ASF356in0.tsv"
+gc = read.table(fileName,sep='\t',header=F,row.names=NULL)
+gcdf = data.frame(gc)
+names(gcdf) = c("time","OD","group")
+t1 = gcdf$time[gcdf$group == 1]
+ODs_356 = collectGrowthCurves(fileName,t1)
+
+timeRef = t1
+for (condition in paste0("ASF356in",c("356","360","361","492","500","502","519")))
+{
+  str(condition)
+  fileName = paste0("/home/glm5uh/asf_characterization/GrowthCurves/SpentMediaGrowthCurveData/",condition,".tsv")
+  new_ODs = collectGrowthCurves(fileName,timeRef=timeRef)
+  ODs_356 = cbind(ODs_356,new_ODs)
 }
 
 
-library(zoo)
+#calculate AUC
+AUCs_356 = apply(ODs_356,2,function(x) sum(diff(timeRef)*rollmean(abs(x-0.001),2)))
+# calculate maxes
+maxes_356 = apply(ODs_356,2,function(x) max(x))
 
-#calculate AUCs
-AUCs = apply(ODs,2,function(x) sum(diff(timeRef)*rollmean(abs(x-0.001),2)))
 
 # Rename rownames in metabolite DF to match AUCs
 for (condition in unique(RI_media_centered_519$Name))
@@ -139,46 +167,110 @@ for (condition in unique(RI_media_centered_519$Name))
   RI_media_centered_519[RI_media_centered_519$Name == condition,]$Name = paste0("ASF",condition) 
 }
 
-# get the top 10 most abundant metabolites based on average change from all conditions
+for (condition in unique(RI_media_centered_356$Name))
+{
+  RI_media_centered_356[RI_media_centered_356$Name == condition,]$Name = paste0("ASF",condition) 
+}
 
-top10 = names(tail(sort(abs(apply(RI_media_centered_519[,-1],2,sd))),15))
-data = RI_media_centered_519
-# Merge to add the AUC value
-data = cbind(data,AUCs[data$Name])
-colnames(data)[length(colnames(data))] = "AUC"
+
+preprocess <- function(integrals,outcomes,outcome_name)
+{
+  #cbind with reordering of labels to match each other
+  data = cbind(integrals,outcomes[integrals$Name])
+  # rename the new column
+  colnames(data)[length(colnames(data))] = outcome_name 
+  # Scale the data. Min/max scaling seems to do the trick, since it preserves information about metabolite consumption/production.
+  data[,!colnames(data) %in% c("Name",outcome_name)] = apply(data[,!colnames(data) %in% c("Name",outcome_name)],2,function(x) (x)/max(abs(x)))
+  #scale the outcome as a z-score
+  data[,outcome_name] = (data[,outcome_name] - mean(data[,outcome_name]))/sd(data[,outcome_name])
+  return(data)
+}
+data_519 = data.frame(preprocess(RI_media_centered_519,AUCs_519,"AUC"))
+data_356 = data.frame(preprocess(RI_media_centered_356,AUCs_356,"AUC"))
+datamax_519 = data.frame(preprocess(RI_media_centered_519,maxes_519,"maxOD"))
+datamax_356 = data.frame(preprocess(RI_media_centered_356,maxes_356,"maxOD"))
 # remove the sample label column
-data = data[,-1]
-# Scale the data. Min/max scaling seems to do the trick, since it preserves information about metabolite consumption/production.
-data = apply(data,2,function(x) (x)/max(abs(x)))
+#data = data[,-1]
+
+# #top10 = names(tail(sort(abs(apply(RI_media_centered_519[,-1],2,sd))),15))
+# data = RI_media_centered_519
+# # Merge to add the AUC value
+# data = cbind(data,AUCs_519[data$Name])
+# colnames(data)[length(colnames(data))] = "AUC"
+# # remove the sample label column
+# data = data[,-1]
+# # Scale the data. Min/max scaling seems to do the trick, since it preserves information about metabolite consumption/production.
+# data = apply(data,2,function(x) (x)/max(abs(x)))
 
 # get the top 10 most variable metabolites
-top10 = names(tail(sort(apply(data[,!(colnames(data) %in% c("AUC",colnames(data)[startsWith(colnames(data),"Unknown")]))],2,sd)),20))
+top10_519 = names(tail(sort(apply(data_519[,!(colnames(data_519) %in% c("AUC","Name",colnames(data_519)[startsWith(colnames(data_519),"Unknown")]))],2,sd)),20))
+top10_356 = names(tail(sort(apply(data_356[,!(colnames(data_356) %in% c("AUC","Name"))],2,sd)),20))
+
+top10_519_max = names(tail(sort(apply(datamax_519[,!(colnames(datamax_519) %in% c("maxOD","Name",colnames(datamax_519)[startsWith(colnames(datamax_519),"Unknown")]))],2,sd)),20))
+top10_356_max = names(tail(sort(apply(datamax_356[,!(colnames(datamax_356) %in% c("maxOD","Name",colnames(datamax_356)[startsWith(colnames(datamax_356),"Unknown")]))],2,sd)),20))
 
 # select only the top 10 metabolites
-data = data[,c(top10,"AUC")]
+data_519 = data_519[,c(top10_519,"AUC","Name")]
+data_356 = data_356[,c(top10_356,"AUC","Name")]
+datamax_519 = datamax_519[,c(top10_519_max,"maxOD","Name")]
+datamax_356 = datamax_356[,c(top10_356_max,"maxOD","Name")]
 
 
 # shortcut: lets use lm()
-data = data.frame(data)
-mod = lm(AUC ~ ., data=data)
-
+mod_519 = lm(AUC ~ . - Name, data=data_519)
 # inspect the fit
-plot(data$AUC,predict.lm(mod,data))
+plot(data_519$AUC,predict.lm(mod_519,data_519))
+
+mod_519_max = lm(maxOD ~ . - Name, data=datamax_519)
+# inspect the fit
+plot(datamax_519$maxOD,predict.lm(mod_519_max,datamax_519))
+
+# for the model, remove the sample with an erroneous spike in OD (ASF356in519_4)
+data_356_prep = data_356[!(data_356$Name == "ASF356in519_4"),]
+datamax_356_prep = datamax_356[!(datamax_356$Name == "ASF356in519_4"),]
+mod_356 = lm(AUC ~ . - Name, data=data_356_prep)
+plot(data_356_prep$AUC,predict.lm(mod_356,data_356_prep))
+
+mod_356_max = lm(maxOD ~ . - Name, data=datamax_356_prep)
+plot(datamax_356_prep$maxOD,predict.lm(mod_356_max,datamax_356_prep))
+
 
 # actually use a bayesian model now
 library(rethinking)
 
 
 # declare the model
+predictors <- reformulate(termlabels = c("a",paste(top10_519,"*",paste0("r",top10_519))), response = mu)[[3]] # [[3]] to get the RHS only
+# construct the prior for each coefficient in the predictor formula
+priors = vector('list',length(top10_519)) #preallocate the list for speed
+for (i in 1:length(top10_519))
+{
+  prior = reformulate(termlabels = "dnorm(-1,1)", response = paste0("r",top10_519[i]))
+  priors[[i]] <- prior
+}
+
+# Give up on handling formulas and just copy/paste the output from predictors into the assignment of mu.
+coef_priors = lapply(paste0("r",top10_519),function(x) reformulate(termlabels = "dnorm(-1,1)", response = x))
 metGauss <- map(
-  alist(
+  append(alist(
     AUC ~ dnorm(mu, sigma),
-    mu <- a+bR*Leucine+bA*Alanine+bG*Unknown39,
+    mu <- a + Leucine * rLeucine + Ethanol * rEthanol + Tryptophan * rTryptophan + Isovalerate * rIsovalerate + Propionate * rPropionate + Valine * rValine + Proline * rProline + Alanine * rAlanine + Methionine * rMethionine + Isoleucine * rIsoleucine + Acetate * rAcetate + Lactate * rLactate + Succinate * rSuccinate + Phenylalanine * rPhenylalanine + Lysine * rLysine + Tyrosine * rTyrosine + Butyrate * rButyrate + Betaine * rBetaine + Glycine * rGlycine + X3.hydroxybutyrate * rX3.hydroxybutyrate,
     a ~ dnorm(-10,10),
-    bR ~ dnorm(-1,1),
-    bA ~ dnorm(-1,1),
-    bG ~ dnorm(-1,1),
     sigma ~ dunif(0,10)
-  ) , 
-  data = data
+  ), priors) , 
+  data = data_519, debug=TRUE, verbose = TRUE
 )
+
+mu = link(metGauss)
+mu.mean = apply(mu,2,mean)
+mu.PI = apply(mu,2,PI)
+auc.sim = sim(metGauss,n=1e4)
+auc.PI = apply(auc.sim, 2 , PI)
+
+plot(mu.mean ~ data_519$AUC, col=rangi2, ylim=range(mu.PI),
+     xlab='Observed AUC', ylab='Predicted AUC')
+abline(a=0,b=1,lty=2)
+for (i in 1:nrow(data_519))
+{
+  lines( rep(data_519$AUC[i],2), c(mu.PI[1,i],mu.PI[2,i]),col=rangi2)
+}
